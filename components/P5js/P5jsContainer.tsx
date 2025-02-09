602b21282f4bcdd4e2aa1dbbf056e4960320e4d4
KwonET <dlxl3652zz@gmail.com>
2025-02-09 22:24:20 +0900

    [0209] home animation2

--- components/P5js/P5jsContainer.tsx
+++ components/P5js/P5jsContainer.tsx
@@ -2,12 +2,34 @@ import { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
 import { setup, draw, windowResized, mousePressed } from './sketch'
 
-const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
-    ssr: false
+// const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
+//     ssr: false
+// })
+
+// export default function P5jsContainer() {
+//     return (<div className="w-full h-[500px]">
+//         <Sketch setup={setup} draw={draw} windowResized={windowResized} mousePressed={mousePressed}
+//         /></div>)
+// }
+
+// react-p5를 import하는 방식 수정
+const Sketch = dynamic(() => import('react-p5'), {
+    ssr: false,
+    loading: () => <div>Loading...</div>
 })
 
 export default function P5jsContainer() {
-    return (<div className="w-full h-[500px]">
-        <Sketch setup={setup} draw={draw} windowResized={windowResized} mousePressed={mousePressed}
-        /></div>)
+    // props를 객체로 전달
+    const props = {
+        setup: setup,
+        draw: draw,
+        windowResized: windowResized,
+        mousePressed: mousePressed
+    }
+
+    return (
+        <div className="w-full h-screen">
+            <Sketch {...props} />
+        </div>
+    )
 }
