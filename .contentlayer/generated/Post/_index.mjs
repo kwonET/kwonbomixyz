12b54e6640ea96abc612b2f68b5e7a74f7bcbfe3
KwonET <dlxl3652zz@gmail.com>
2025-03-30 22:28:20 +0900

    feat : blog8 content post

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -7,5 +7,6 @@ import blog4Mdx from './blog4.mdx.json' assert { type: 'json' }
 import blog5Mdx from './blog5.mdx.json' assert { type: 'json' }
 import blog6Mdx from './blog6.mdx.json' assert { type: 'json' }
 import blog7Mdx from './blog7.mdx.json' assert { type: 'json' }
+import blog8Mdx from './blog8.mdx.json' assert { type: 'json' }
 
-export const allPosts = [blog1Mdx, blog2Mdx, blog3Mdx, blog4Mdx, blog5Mdx, blog6Mdx, blog7Mdx]
+export const allPosts = [blog1Mdx, blog2Mdx, blog3Mdx, blog4Mdx, blog5Mdx, blog6Mdx, blog7Mdx, blog8Mdx]
