f42663f23190ffadeead9026bd22059c6fc13de7
KwonET <dlxl3652zz@gmail.com>
2025-04-11 17:11:51 +0900

    fix : type error

--- components/P5js/SandBox.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-interface SandBoxProps {
-    running: boolean;
-    result: string;
-    cellSize?: number;
-    colorPair?: string[];
-}
-declare const SandBox: ({ running, result, cellSize, colorPair }: SandBoxProps) => JSX.Element;
-export default SandBox;
